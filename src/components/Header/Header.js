8e6b91a5f5bf6ad624c6d63ef9ee5c346db8efbe
kanykei7 <duishenalievak@gmail.com>
2021-04-29 18:23:40 +0600

    card

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,20 +2,21 @@ import React from "react";
 import "./Header.css"
 import rolexLogo from "../../assets/image/Rolex-logo.png"
 import videoMP4 from "../../assets/video/new-datejust-cover-video.webm"
+import shoppingCart from "../../assets/image/shopping.svg"
 
 const componentName = () => {
   
   return (
     
-  <div>
+  <div className="popi">
     <div className="header">
                 <div className="container">
                   <div className="main-header">
                       <div className="left-side">
-                          <h5><a href="#">Меню</a></h5>
-                          <h5><a href="#">Часы</a></h5>
-                          <h5><a href="#">Мир Rolex</a></h5>
-                          <h5><a href="#">Поиск дистрибьютора</a></h5>
+                          <p className="header-p"><a href="#">Меню</a></p>
+                          <p className="header-p"><a href="#">Часы</a></p>
+                          <p className="header-p"><a href="#">Мир Rolex</a></p>
+                          <p className="header-p"><a href="#">Поиск дистрибьютора</a></p>
                       <div className="mid">
                         <img src={rolexLogo} alt="rolex-logo"/>
                         <h2>ROLEX</h2>
@@ -24,11 +25,11 @@ const componentName = () => {
                       <div className="right-side">
                           <div className="icon">
                               <i className="fas fa-search"></i>
-                              <h5><a href="#">Поиск</a></h5>
+                              <p className="header-p"><a href="#">Поиск</a></p>
                           </div>
                           <div className="icon">
                               <i className="far fa-heart"></i>
-                              <h5><a href="#">Ваша подборка</a></h5>
+                              <p className="header-p"><a href="#">Ваша подборка</a></p>
                           </div>
                       </div>
                   </div>
@@ -39,6 +40,21 @@ const componentName = () => {
                 <source src={videoMP4} type="video/webm"/>
             </video>
             </div>
+            {/* ---------------------START-CARD--------------------- */}
+            <div className="cart">
+              <img src="https://content.rolex.com/dam/new-watches-2021/homepage/roller/all-watches/watches_0003_m126200-0020-datejust.jpg?imwidth=550"/>
+              <div className="alert">
+                <h3>DATEJUST</h3>
+                <p>Эталон классических часов</p>
+                <div class="price-shop">
+                <p>1300$</p>
+                <button className="shop-btn">
+                  <img className="shoppingCart" src={shoppingCart}/>
+                </button>
+                </div>
+              </div>
+            </div>
+            {/* ----------------------END-CARD-------------------- */} 
   </div>
   )
 };

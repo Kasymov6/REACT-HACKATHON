3a9dbfb181c16de529608f9cbc80cb506db4b09a
kanykei7 <duishenalievak@gmail.com>
2021-04-29 11:15:18 +0600

    signin signup

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
 import AuthContextProvider from "./context/AuthContext";
 const Routes = () => {
   return (
     <AuthContextProvider>
       <BrowserRouter>
-        <Switch></Switch>
+        <Switch>
+        <Route exact path="/login" component={SignIn}/>
+        <Route exact path="/signup" component={SignUp}/>
+        </Switch>
       </BrowserRouter>
     </AuthContextProvider>
   );

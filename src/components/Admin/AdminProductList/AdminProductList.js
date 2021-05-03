ff9d707d68490c20cad780d6001051566b45f145
nurmuhammed <nur.janybaev1@gmail.com>
2021-05-03 11:12:39 +0600

    styagivanie

--- src/components/Admin/AdminProductList/AdminProductList.js
+++ src/components/Admin/AdminProductList/AdminProductList.js
@@ -4,20 +4,20 @@ import ProductCard from "../../Products/ProductCard";
 import "../../Products/ProductList.css";
 import AdminProductCard from "../AdminProductCard/AdminProductCard";
 const AdminProductList = () => {
-  const { getProducts, productsData } = useContext(productContext);
-  useEffect(() => {
-    getProducts();
-  }, []);
+    const { getProducts, productsData } = useContext(productContext);
+    useEffect(() => {
+        getProducts();
+    }, []);
 
-  return (
-    <div className="wrap">
-      <div className="list">
-        {productsData.map((item) => (
-          <AdminProductCard key={item.id} item={item} />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="wrap">
+            <div className="list">
+                {productsData.map((item) => (
+                    <AdminProductCard key={item.id} item={item} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default AdminProductList;
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -11,39 +11,46 @@ const reducer = (state = INIT_STATE, action) => {
     }
 };
 const AuthContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, INIT_STATE);
-  async function registerUser(e, history) {
-    e.preventDefault();
-    const newUser = {
-      email: e.target[0].value,
-      password: e.target[2].value,
-    };
-    console.log(e);
-    try {
-      const res = await axios.post(`${AUTH_API}/api/auth/register`, newUser);
-      history.push("/login");
-    } catch (err) {
-      console.log(err.response);
-    }
-
-    async function loginUser(e, history) {
+    const [state, dispatch] = useReducer(reducer, INIT_STATE);
+    async function registerUser(e, history) {
         e.preventDefault();
-        const user = {
+        const newUser = {
             email: e.target[0].value,
             password: e.target[2].value,
         };
+        console.log(e);
         try {
-            const data = await axios.post(`${AUTH_API}/api/auth/login`, user);
-            console.log(data);
-            history.push("/");
+            const res = await axios.post(
+                `${AUTH_API}/api/auth/register`,
+                newUser
+            );
+            history.push("/login");
         } catch (err) {
-            alert(err.response.data.message);
+            console.log(err.response);
         }
+
+        async function loginUser(e, history) {
+            e.preventDefault();
+            const user = {
+                email: e.target[0].value,
+                password: e.target[2].value,
+            };
+            try {
+                const data = await axios.post(
+                    `${AUTH_API}/api/auth/login`,
+                    user
+                );
+                console.log(data);
+                history.push("/");
+            } catch (err) {
+                alert(err.response.data.message);
+            }
+        }
+        return (
+            <authContext.Provider value={{ registerUser, loginUser }}>
+                {children}
+            </authContext.Provider>
+        );
     }
-    return (
-        <authContext.Provider value={{ registerUser, loginUser }}>
-            {children}
-        </authContext.Provider>
-    );
 };
 export default AuthContextProvider;

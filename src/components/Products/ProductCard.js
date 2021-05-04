172517c9e63753deadb9fd86ddca49964deebe0f
kanykei7 <duishenalievak@gmail.com>
2021-05-04 11:56:20 +0600

    i wanna save popo

--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import "./ProductCard.css";
 import { Link } from "react-router-dom";
-// import shoppingCart from "../../assets/image/shopping.svg";
+import shoppingCart from "../../assets/image/shopping.svg";
 import { productContext } from "../../context/ProductContext";
 
 const ProductCard = (props, {item}) => {
@@ -11,14 +11,14 @@ const ProductCard = (props, {item}) => {
     <div className="cart">
       <img src={props.item.img} />
       <div className="alert">
-        <h3>{props.item.title}</h3>
+        <h3 className="cardTitle">{props.item.title}</h3>
+          <p className="cardPrice">{props.item.price}$</p>
         <div className="price-shop">
-          <p>{props.item.price}$</p>
           <Link to={`/details/${props.item.id}`}>
-            <button>Подробнее</button>
+            <button className="infoMore">Подробнее</button>
           </Link>
           
-          <button onClick={()=>addProductToCart(props.item)} className="shop-btn">add
+          <button onClick={()=>addProductToCart(props.item)} className="shop-btn"><img className="shoppingCart" src={shoppingCart}/>
           </button>
         </div>
       </div>
